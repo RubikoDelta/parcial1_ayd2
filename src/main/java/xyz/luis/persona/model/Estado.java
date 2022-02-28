2ec1161b555a466ae8875ba332e4f8a21b3219c6
RubikoDelta <luislobosmonroy98@gmail.com>
2022-02-28 16:56:31 -0600

    M

--- src/main/java/xyz/luis/persona/model/Estado.java
+++ src/main/java/xyz/luis/persona/model/Estado.java
@@ -20,44 +20,5 @@ public class Estado implements Serializable {
 	@JoinColumn(name="id_pais")
 	private Pais pais;
 	private String nombre;
-	
-	public Estado() {
-	}
-	
-	
-	public Estado(Pais pais, String nombre) {
-		super();
-		this.pais = pais;
-		this.nombre = nombre;
-	}
-	
-	
-	public Long getId() {
-		return id;
-	}
-
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-
-	public Pais getPais() {
-		return pais;
-	}
-	public void setPais(Pais pais) {
-		this.pais = pais;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
-	
-	
-	
-	
 
 }
--- src/main/java/xyz/luis/persona/model/Pais.java
+++ src/main/java/xyz/luis/persona/model/Pais.java
@@ -15,28 +15,7 @@ public class Pais implements Serializable {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String nombre;
-	
-	public Pais() {
-	}
-	
-	public Pais(String nombre) {
-		super();
-		this.nombre = nombre;
-	}
-	
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+
 	
 	
 	
